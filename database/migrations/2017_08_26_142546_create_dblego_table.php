0ab51b826c71a8f3de2100c4148a24399c80746b
Simone Bissi <ilsimonone@gmail.com>
2017-09-04 15:16:57 +0200

    Update 2017_08_26_142546_create_dblego_table.php
    column name

--- database/migrations/2017_08_26_142546_create_dblego_table.php
+++ database/migrations/2017_08_26_142546_create_dblego_table.php
@@ -14,17 +14,26 @@ class CreateDblegoTable extends Migration
     public function up()
     {
         Schema::create('ldblegos', function (Blueprint $table) {
+            //ID
             $table->increments('id');
+            //NAMEDB
             $table->string('namedb',128);
+            //USER_ID
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->on('lusers')->references('id')->onDelete('cascade')->onUpdate('cascade');
+            //PART
             $table->string('part',20);
             $table->foreign('part')->on('lparts')->references('part_num');
+           //COLOR
             $table->integer('color')->unsigned();
             $table->foreign('color')->on('lcolors')->references('col_num');
+            //UNIQUE
             $table->unique(array('user_id', 'part', 'color'));
+            //QUANTITY
             $table->integer('quantity')->unsigned();
+            //LOCATION
             $table->string('location',128);
+            //time
             $table->softDeletes();
             $table->timestamps();
         });
