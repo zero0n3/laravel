5d1f896945a86b34c0feafe5c7aca89c33a008c4
Simone Bissi <ilsimonone@gmail.com>
2017-09-04 15:12:43 +0200

    Update 2017_09_02_213931_create_moc_table.php
    colonne

--- database/migrations/2017_09_02_213931_create_moc_table.php
+++ database/migrations/2017_09_02_213931_create_moc_table.php
@@ -14,16 +14,24 @@ class CreateMocTable extends Migration
     public function up()
     {
         Schema::create('lmocs', function (Blueprint $table) {
+            //ID
             $table->increments('id');
+            //NOMEMOC
             $table->string('namemoc',128);
+            //USER_ID
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->on('lusers')->references('id')->onDelete('cascade')->onUpdate('cascade');
+            //PART
             $table->string('part',20);
             $table->foreign('part')->on('lparts')->references('part_num');
+            //COLOR
             $table->integer('color')->unsigned();
             $table->foreign('color')->on('lcolors')->references('col_num');
+            //UNIQUE FIELDS
             $table->unique(array('user_id', 'part', 'color'));
+            //QUANTITY
             $table->integer('quantity')->unsigned();
+            //times
             $table->softDeletes();
             $table->timestamps();
         });
