02d161138c9fc4d60a982a3d40e175076787db16
Simone Bissi <ilsimonone@gmail.com>
2017-09-11 10:56:56 +0200

    Update parts.blade.php

--- resources/views/lego/parts.blade.php
+++ resources/views/lego/parts.blade.php
@@ -12,16 +12,27 @@
   <table class="table table-striped">
     <tr>
       <th>PART</th>
-      <th>DESCRIPTION</th>
-      <th>IMG 1</th>
-      <th>WHERE</th>
-      <th>QTA</th>
       <th>COLOR</th>
-      <th>PEER1</th>
-      <th>API</th>
-      <th>X</th>
+      <th>IMG 1</th>
+      <th>IMG 2</th>
     </tr>
-
+   @forelse ($lparts as $lpart)
+      <tr>
+        <td>{{$lpart->part}}</td>
+        <td>{{$lpart->color}}</td>
+        <td><img src="http://bricker.info/images/parts/{{$lpart->part}}.jpg" onerror=\"this.src = '//:0';\" width='42'></td>
+        <td><img src="https://m.rebrickable.com/media/parts/ldraw/{{$lpart->color}}/{{$lpart->part}}.png" onerror=\"this.src = '//:0';\" width='42'></td>
+        <?php
+            //$res = $client->request('GET', 'http://rebrickable.com/api/v3/lego/parts/'.$lpart->part.'/colors/'.$lpart->color.'/?key=BzyyfQneul');
+            //$data = json_decode($res->getBody(), true);
+            //echo "<img src='".$data['part_img_url']."' onerror=\'this.src = '//:0';\' width='42'></td>";
+        ?>
+      </tr>
+    @empty
+      <tr>
+        <td colspan="5">no PARTS</td>
+      </tr>
+    @endforelse
 
 @endsection
 
