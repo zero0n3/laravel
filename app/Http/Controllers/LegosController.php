b4c7dcfa5a44a6ff0011a2828d0a139ba1c3230a
Simone Bissi <ilsimonone@gmail.com>
2017-10-09 10:20:41 +0200

    Update LegosController.php

--- app/Http/Controllers/LegosController.php
+++ app/Http/Controllers/LegosController.php
@@ -143,7 +143,7 @@ class LegosController extends Controller
     	$queryBuilder = Lpart::orderBy('part_num','asc');
     	    
     	$lparts = $queryBuilder->paginate(10);
-            
+        dd($lparts);   
     	return view('lego.imgparts', ['lparts' => $lparts]);
        
         
